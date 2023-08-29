36e6723fbcf3efb55559b4a3c2361c02fd48dcac
SSS BBB <sssbbbkaow@gmail.com>
2023-08-29 23:44:13 +0700

    Add winning system

--- src/frames/GamePanel.java
+++ src/frames/GamePanel.java
@@ -6,6 +6,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.util.Arrays;
 
 // The coordinates of the frame include the top bar, so in order to avoid confusion we need to create a panel.
 public class GamePanel extends JPanel implements MouseListener {
@@ -29,20 +30,55 @@ public class GamePanel extends JPanel implements MouseListener {
 
     public static int thick = 3;
 
+    private JLabel winnerLabel;
+    private JLabel turnLabel;
+    private boolean clicked = true;
+
     public GamePanel() {
         this.setPreferredSize(new Dimension(GameFrame.WIDTH, GameFrame.HEIGHT));
         this.addMouseListener(this);
+        this.setLayout(null);
         system = new GameSystem();
+
+//        JLabel label = new JLabel("My name is Suphawit.");
+//        label.setBounds(0, 0, w, h);
+//        label.setFont(new Font("Arial", Font.PLAIN, 24));
+//        label.setHorizontalAlignment(JLabel.CENTER);
+//        label.setVerticalAlignment(JLabel.CENTER);
+//        add(label);
+
+        winnerLabel = new JLabel();
+        winnerLabel.setBounds(0, 0, w, h);
+        winnerLabel.setFont(new Font("Arial", Font.PLAIN, 48));
+        winnerLabel.setForeground(Color.RED);
+        winnerLabel.setHorizontalAlignment(JLabel.CENTER);
+        winnerLabel.setVerticalAlignment(JLabel.CENTER);
+        winnerLabel.setVisible(false);
+        add(winnerLabel);
+
+        turnLabel = new JLabel("Turn: " + system.getCurrentTurn());
+        turnLabel.setBounds(0, 0, w, h);
+        turnLabel.setFont(new Font("Arial", Font.PLAIN, 24));
+        turnLabel.setHorizontalAlignment(JLabel.CENTER);
+        turnLabel.setVerticalAlignment(JLabel.TOP);
+        add(turnLabel);
     }
 
     @Override
     public void paintComponent(Graphics g) {
+        // make sure to paint only after mouse was clicked
+        if (!clicked) {
+            return;
+        }
+        super.paintComponent(g); // clear the screen
+        clicked = false;
 
         Graphics2D g2d = (Graphics2D) g;
         g2d.setStroke(new BasicStroke(thick));
 
 
         if (!system.isEnd()) {
+
             // horizontal lines
             g2d.drawLine(startX, y1, endX, y1);
             g2d.drawLine(startX, y2, endX, y2);
@@ -51,76 +87,165 @@ public class GamePanel extends JPanel implements MouseListener {
             g2d.drawLine(x1, startY, x1, endY);
             g2d.drawLine(x2, startY, x2, endY);
 
+
             // draw X or O
-            if (system.getBox()[0] != 0 && system.getBox()[1] != 0) {
-                // x
-                int move = 20;
-                int l1x1 = 0;
-                int l1y1 = 0;
-                int l1x2 = 0;
-                int l1y2 = 0;
-
-                int l2x1 = 0;
-                int l2y1 = 0;
-                int l2x2 = 0;
-                int l2y2 = 0;
-
-                if (system.getBox()[0] == 1) {
-                    l1x1 = startX + move;
-                    l1x2 = x1 - move;
-
-                    l2x1 = startX + move;
-                    l2x2 = x1 - move;
+            for (int row = 0; row < system.getBoard().length; row++) {
+                for (int col = 0; col < system.getBoard()[row].length; col++) {
+                    drawSign(row, col, g2d);
                 }
-                else if (system.getBox()[0] == 2) {
-                    l1x1 = x1 + move;
-                    l1x2 = x2 - move;
-
-                    l2x1 = x1 + move;
-                    l2x2 = x2 - move;
-                }
-                else {
-                    l1x1 = x2 + move;
-                    l1x2 = endX - move;
-
-                    l2x1 = x2 + move;
-                    l2x2 = endX - move;
-                }
-
-                if (system.getBox()[1] == 1) {
-                    l1y1 = startY + move;
-                    l1y2 = y1 - move;
-
-                    l2y1 = y1 - move;
-                    l2y2 = startY + move;
-                }
-                else if (system.getBox()[1] == 2) {
-                    l1y1 = y1 + move;
-                    l1y2 = y2 - move;
-
-                    l2y1 = y2 - move;
-                    l2y2 = y1 + move;
-                }
-                else {
-                    l1y1 = y2 + move;
-                    l1y2 = endY - move;
-
-                    l2y1 = endY - move;
-                    l2y2 = y2 + move;
-                }
-
-                g2d.setStroke(new BasicStroke(thick));
-                g2d.drawLine(l1x1, l1y1, l1x2, l1y2);
-                g2d.drawLine(l2x1, l2y1, l2x2, l2y2);
             }
+            turnLabel.setText("Turn: " + system.getCurrentTurn());
+            showBoard(system.getBoard());
+            System.out.println("-------------------");
+            system.checkWin();
+
+            if (system.isEnd()) {
+                winnerLabel.setText("The winner is " + system.getWinner());
+                winnerLabel.setVisible(true);
+                turnLabel.setVisible(false);
+            }
+        }
+    }
+
+    public void drawSign(int row, int col, Graphics2D g2d) {
+        int margin = 40;
+        if (system.getBoard()[row][col].equals("X")) {
+            // x sign
+            int l1x1 = 0;
+            int l1y1 = 0;
+            int l1x2 = 0;
+            int l1y2 = 0;
+
+            int l2x1 = 0;
+            int l2y1 = 0;
+            int l2x2 = 0;
+            int l2y2 = 0;
+
+            // col 1
+            if (col == 0) {
+                l1x1 = startX + margin;
+                l1x2 = x1 - margin;
+
+                l2x1 = startX + margin;
+                l2x2 = x1 - margin;
+            }
+
+            // col 2
+            else if (col == 1) {
+                l1x1 = x1 + margin;
+                l1x2 = x2 - margin;
+
+                l2x1 = x1 + margin;
+                l2x2 = x2 - margin;
+            }
+
+            // col 3
+            else {
+                l1x1 = x2 + margin;
+                l1x2 = endX - margin;
+
+                l2x1 = x2 + margin;
+                l2x2 = endX - margin;
+            }
+
+            // row 1
+            if (row == 0) {
+                l1y1 = startY + margin;
+                l1y2 = y1 - margin;
+
+                l2y1 = y1 - margin;
+                l2y2 = startY + margin;
+            }
+
+            // row 2
+            else if (row == 1) {
+                l1y1 = y1 + margin;
+                l1y2 = y2 - margin;
+
+                l2y1 = y2 - margin;
+                l2y2 = y1 + margin;
+            }
+
+            // row 3
+            else {
+                l1y1 = y2 + margin;
+                l1y2 = endY - margin;
+
+                l2y1 = endY - margin;
+                l2y2 = y2 + margin;
+            }
+
+            g2d.setStroke(new BasicStroke(thick));
+            g2d.drawLine(l1x1, l1y1, l1x2, l1y2);
+            g2d.drawLine(l2x1, l2y1, l2x2, l2y2);
+        }
+        else if (system.getBoard()[row][col].equals("O")) {
+            // O sign
+            int x_center = 0;
+            int y_center = 0;
+            int d = ((int) Math.round(line_size_h * 0.33)) - margin; // diameter
+            // int r = ((int) Math.round((x1 - startX) * 0.5)) - margin; // radius
+
+            // col 1
+            if (col == 0) {
+                x_center = (startX + x1) / 2;
+            }
+
+            // col 2
+            else if (col == 1) {
+                x_center = (x1 + x2) / 2;
+            }
+
+            // col 3
+            else {
+                x_center = (x2 + endX) / 2;
+            }
+
+            // row 1
+            if (row == 0) {
+                y_center = (startY + y1) / 2;
+            }
+
+            // row 2
+            else if (row == 1) {
+                y_center = (y1 + y2) / 2;
+            }
+
+            // row 3
+            else {
+                y_center = (y2 + endY) / 2;
+            }
+
+            // draw circle
+            g2d.drawOval(x_center - d/2, y_center - d/2, d, d);
+
+        }
+    }
+
+    public void showBoard(String[][] board) {
+        for (int row = 0; row < board.length; row++) {
+            for (int col = 0; col < board[row].length; col++) {
+                System.out.print(board[row][col]);
+            }
+            System.out.println();
         }
     }
 
     @Override
     public void mouseClicked(MouseEvent e) {
-        system.checkBox(e.getX(), e.getY());
-        System.out.println(String.valueOf(system.getBox()[0]) + ", " + String.valueOf(system.getBox()[1]));
-        repaint();
+        if (!system.isEnd()) {
+            system.checkBox(e.getX(), e.getY());
+            // System.out.println(String.valueOf(system.getCurrentInput()[0]) + ", " + String.valueOf(system.getCurrentInput()[1]));
+            clicked = true;
+            if (system.getCurrentTurn() == "X") {
+                system.setCurrentTurn("O");
+            }
+            else {
+                system.setCurrentTurn("X");
+            }
+
+            repaint();
+        }
     }
 
     @Override
--- src/game_controllers/GameSystem.java
+++ src/game_controllers/GameSystem.java
@@ -1,46 +1,85 @@
 package game_controllers;
 
-import frames.GamePanel;
-
-import java.awt.*;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
+import java.util.Arrays;
 
 import static frames.GamePanel.*;
 
 public class GameSystem {
     private boolean end = false;
-    private int[] box = {0, 0};
+    private final int[] currentInput = {0, 0};
+    private String currentTurn = "X";
 
-    public void setBox(int[] box) {
-        this.box = box;
+    /*
+           {
+             {O, X, O}.
+             {X, O, O},
+             {O, X, X}
+           }
+        */
+    private String[][] board = new String[3][3];
+    private final String EMPTYSIGN = "-";
+
+    private String winner = EMPTYSIGN;
+
+    public GameSystem() {
+        resetBoard();
+    }
+
+    public void resetBoard() {
+        // board = new String[][]{{"E", "E", "E"}, {"E", "E", "E"}, {"E", "E", "E"}}; // E stands for Empty
+        for (int row = 0; row < board.length; row++) {
+            for (int col = 0; col < board[row].length; col++) {
+                board[row][col] = EMPTYSIGN;
+            }
+        }
     }
 
     public int[] checkBox(int x, int y) {
 
-        // check x
+        // check x (column)
         if (x >= startX && x <= x1) {
-            box[0] = 1;
+            currentInput[1] = 1;
         }
         else if (x >= x1 && x <= x2) {
-            box[0] = 2;
+            currentInput[1] = 2;
         }
         else if (x >= x2 && x <= endX) {
-            box[0] = 3;
+            currentInput[1] = 3;
         }
 
-        // check y
+        // check y (row)
         if (y >= startY && y <= y1) {
-            box[1] = 1;
+            currentInput[0] = 1;
         }
         else if (y >= y1 && y <= y2) {
-            box[1] = 2;
+            currentInput[0] = 2;
         }
         else if (y >= y2 && y <= endY) {
-            box[1] = 3;
+            currentInput[0] = 3;
         }
 
-        return box;
+        board[currentInput[0] - 1][currentInput[1] - 1] = currentTurn;
+        return currentInput;
+    }
+
+    public void checkWin() {
+        // check for horizontal win
+        for (int row = 0; row < board.length; row++) {
+            if (!board[row][0].equals(EMPTYSIGN)) {
+                if (board[row][0].equals(board[row][1]) && board[row][0].equals(board[row][2])) {
+                    winner = board[row][0];
+                    end = true;
+                    return;
+                }
+            }
+        }
+
+        // check for vertical win
+        for (int col = 0; col < board[0].length; col++) {
+
+        }
+
+        end = false;
     }
 
     public boolean isEnd() {
@@ -51,12 +90,28 @@ public class GameSystem {
         this.end = end;
     }
 
-    public int[] getBox() {
-        return box;
+    public int[] getCurrentInput() {
+        return currentInput;
     }
 
-    public void resetBox() {
-        box[0] = 0;
-        box[1] = 0;
+    public void resetInput() {
+        currentInput[0] = 0;
+        currentInput[1] = 0;
+    }
+
+    public String getCurrentTurn() {
+        return currentTurn;
+    }
+
+    public void setCurrentTurn(String currentTurn) {
+        this.currentTurn = currentTurn;
+    }
+
+    public String[][] getBoard() {
+        return board;
+    }
+
+    public String getWinner() {
+        return winner;
     }
 }
